4dfa0767f5f964535a50c84bf6a7996fb362b30d
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-08-10 01:19:03 +0800

    [Add] Dynamic loading of activities

---
+++ src/constants/courses/carActivitiesData.ts
@@ -0,0 +1,18 @@
+export const carActivitiesData = [
+  {
+    id: '0',
+    title: 'Introduction to Robotics',
+    activities: [
+      { title: 'Wake Up, Robot!', description: 'Learn to turn on and connect to your robot car' },
+      { title: 'First Steps Forward', description: 'Make your robot car move forward for the first time' },
+    ],
+  },
+  {
+    id: '1',
+    title: 'Advanced Navigation',
+    activities: [
+      { title: 'Spin and Turn', description: 'Learn how to make your robot car turn in place' },
+      { title: 'Navigate an Obstacle', description: 'Guide your robot car around a simple obstacle' },
+    ],
+  },
+];
