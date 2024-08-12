934b84d595de440a01b014e5dc7ca25bb2a49588
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-08-13 01:32:09 +0800

    [Refactor] courses page

--- src/components/custom/sub-components/Icons.tsx
+++ src/components/custom/sub-components/Icons.tsx
@@ -309,3 +309,25 @@ export function MonitorIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElem
     </svg>
   );
 }
+
+export function CarIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect x="3" y="11" width="18" height="7" rx="2" ry="2" />
+      <path d="M5 11V7a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v4" />
+      <circle cx="7.5" cy="16.5" r="1.5" />
+      <circle cx="16.5" cy="16.5" r="1.5" />
+    </svg>
+  );
+}
